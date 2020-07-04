262582789bb5eb45cf3524c65c17bdd9ae20e11b
MONISH SHAIKH <67359415+MonisVibes@users.noreply.github.com>
2020-07-04 10:40:46 +0530

    new files

---
+++ task7/script.js
@@ -0,0 +1,17 @@
+//my current age
+const myAge = 20;
+//early Years 
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *=4;
+
+console.log(laterYears);
+console.log(earlyYears);
+
+//my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+//my name and age in dog years
+let MyName = “Monish”.toLowerCase();
+console.log('My name is MyName. I am myAgeInDogYears years old in dog years.') ;
+
