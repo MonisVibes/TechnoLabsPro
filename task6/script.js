3747bbb176b3945acc7d56502ed433fbffc699df
MONISH SHAIKH <67359415+MonisVibes@users.noreply.github.com>
2020-07-08 13:03:49 +0530

    new files

--- task6/script.js
+++ task6/script.js
@@ -6,5 +6,10 @@ const celsius = kelvin - 273;
 let fahrenheit = celsius * (9/5) + 32;
 //Rounding down fahrenheit
 Fahrenheit = Math.floor(fahrenheit);
-console.log('The temperature is fahrenheit degrees fahrenheit');
+console.log('The temperature is ${fahrenheit} degrees fahrenheit.');
 let Newton = celsius * (33/100);
+Newton = Math.floor(Newton); 
+console.log('The temperature is {Newton} degrees Newton.');
+
+
+
--- task7/script.js
+++ task7/script.js
@@ -9,9 +9,11 @@ laterYears *=4;
 console.log(laterYears);
 console.log(earlyYears);
 
-//my age in dog years
-let myAgeInDogYears = earlyYears + laterYears;
-//my name and age in dog years
-let MyName = “Monish”.toLowerCase();
-console.log('My name is MyName. I am myAgeInDogYears years old in dog years.') ;
+   //my age in dog years
+   let myAgeInDogYears= earlyYears + laterYears;
+   //my name and age in dog years
+   let MyName = “Enikardie”.toLowerCase();
+   console.log(My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.);
+   
+
 
