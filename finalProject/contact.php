1c9405819bdd536f6c2664b1d99cd609491a18f3
MONISH SHAIKH <vip71169@gmail.com>
2020-08-05 10:06:10 +0530

    Update contact.php

--- finalProject/contact.php
+++ finalProject/contact.php
@@ -6,19 +6,26 @@ $contact = $_REQUEST['phone_no'];
 $Message = $_REQUEST['Message'];
 
 //check input field
-if(empty($name) || empty($email) || empty($contact) || empty($Message))
+if(empty($name) || empty($email) || empty($contact))
 {
-    echo "please fill all the fields"
+    echo "please fill all the fields";
 } 
 else
 {
-    mail("technocolabs@gmail.com ", "Technocolabs Indore" , $Message , "From: $name
+    $body="Name :".$name."\nEmail :".$email."\nContact : ".$contact."\nMessage: ".$Message;
+    mail("monish.vibes@gmail.com ", "Technocolabs Indore" , $body , "From: $name
     <$email>" );
+    mail($email, "Technocolabs Indore","Thanks you for contact us,\nOur team soon response you","From: monish.vibes@gmail.com");
 
-    echo "<script type= 'text/javascrip'>alert('your message sent successfully');
-       
-         window.history.log(-1);
-    
+    echo "<script>alert('your message sent successfully');
     </script>";
+    header("location: index.html");
 }
 ?>
+
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    location = 'http://google.com/thank-you';
+}, false );
+</script>
+
